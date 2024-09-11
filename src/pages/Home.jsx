ee020d81cc7c15acc07168ee19c535df432dfdcf
Islom Pardaboyev <ipardaboyev574@gmail.com>
2024-09-11 19:24:36 +0500

    init

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,37 +9,40 @@ function Home() {
     queryFn: () => axios.get("http://localhost:3000/users"),
   });
 
-  const { mutate:addUser } = useMutation({
+  const { mutate: addUser } = useMutation({
     mutationFn: (body) => axios.post("http://localhost:3000/users", body),
     onSuccess: () => {
       refetch();
     },
   });
 
-  const {mutate:deleteUser} = useMutation({
+  const { mutate: deleteUser } = useMutation({
     mutationFn: (id) => axios.delete(`http://localhost:3000/users/${id}`),
-    onSuccess: () => refetch()
-  })
+    onSuccess: () => refetch(),
+  });
 
-  const {mutate: updateUser} = useMutation({
-    mutationFn: (data) => axios.patch(`http://localhost:3000/users/${data.id}`, {
-      name: data.newValue
-    }),
-    onSuccess:() => refetch()
-  })
+  const { mutate: updateUser } = useMutation({
+    mutationFn: (data) =>
+      axios(`http://localhost:3000/users/${data.id}`, {
+        name: data.newValue,
+      }),
+    onSuccess: () => refetch(),
+  });
 
   function handleSubmit(e) {
     e.preventDefault();
     const data = {
-      id: String(userData?.data?.length
-        ? Number(userData?.data[userData?.data?.length - 1]?.id + 1)
-        : "1"),
+      id: String(
+        userData?.data?.length
+          ? Number(userData?.data[userData?.data?.length - 1]?.id) + 1
+          : "1"
+      ),
       name: e.target.user.value,
       lastName: "Vohidov",
       age: 20,
     };
     console.log(userData?.data?.length);
-  
+
     console.log(data);
     addUser(data);
   }
@@ -63,15 +66,28 @@ function Home() {
             <p>Name: {item.name}</p>
             <p>Last Name: {item.lastName}</p>
             <p>Age: {item.age}</p>
-            <Button onClick={() => deleteUser(item.id)} size="large"  className="!bg-red-600 !text-white">Delete</Button>
-            <Button onClick={() => {
-              let newValue = prompt('Enter new value', item.name)
-              const data = {
-                id: item.id,
-                newValue
-              }
-              updateUser(data)
-            }} className="!bg-green-500">Update</Button>
+            <Button
+              onClick={() => deleteUser(item.id)}
+              size="large"
+              className="!bg-red-600 !text-white"
+            >
+              Delete
+            </Button>
+            <Button
+              onClick={() => {
+                let newValue = prompt("Enter new value", item.name);
+                const data = {
+                  id: item.id,
+                  newValue,
+                };
+                updateUser(data);
+              }}
+              className="!bg-green-500"
+              size="large"
+              classNames={"!bg-green-500"}
+            >
+              Update
+            </Button>
           </div>
         ))}
       </div>
